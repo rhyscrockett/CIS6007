283e91e86d9f685a449a50883cd847d2958bfed7
Rhys Crockett <rhysmorgancrockett@gmail.com>
2021-12-01 11:29:50 +0000

    Added the function to generate random values between 1 and 50 then print 6 of them to the screen.

--- Assessment/LotteryNumberGenerator.cpp
+++ Assessment/LotteryNumberGenerator.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
 
+#include "IRandom.h"
+#include "NumberStorage.h"
+
+template<typename T>
+void print(const std::vector<T>& v, const int& limit) {
+    if (v.empty())
+    return;
+    for (auto i = 0; i < limit; ++i)
+    std::cout << v[i] << " ";
+    std::cout << std::endl;
+}
+
 int main () {
-    std::cout << "Hello World" << std::endl;
+    std::cout << "Hello World" << std::endl; // test
+    IRandom rand(1, 50); // generate value between 1 and 50
+
+    std::vector<int> data;
+    for (auto i = 0; i < 500; ++i) {
+        int n = rand();
+        data.push_back(n);
+    }
+    print(data, 6);
 }
---
+++ Assessment/NumberStorage.h
@@ -0,0 +1,4 @@
+
+struct numbers {
+    std::vector<int> ticket;
+};
