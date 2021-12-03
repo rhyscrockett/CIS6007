a646f2f3becebab9f8f72a12e3c2eb5d2d4fddcb
Rhys Crockett <rhysmorgancrockett@gmail.com>
2021-12-03 16:22:03 +0000

    Added the async execution times for half and quaters, added the result table aswell.

--- workshops/week4/2.cpp
+++ workshops/week4/2.cpp
@@ -24,10 +24,41 @@ double sumVector(const std::vector<int>& v) {
     return total;
 }
 
+double sumVectorA(int* start, int* end, int init) {
+    double total = std::accumulate(start, end, init);
+    return total;
+}
+
+double sumVectorAsyncH(std::vector<int>& v) {
+    auto start = &v[0];
+    auto size = v.size();
+    auto end = start + size;
+
+    auto sumVector1 = std::async(std::launch::async, sumVectorA, start, (start + size / 2), 0);
+    auto sumVector2 = std::async(std::launch::async, sumVectorA, (start + size / 2), (start + size), 0);
+
+    double total = sumVector1.get() + sumVector2.get();
+    return total;
+}
+
+double sumVectorAsyncQ(std::vector<int>& v) {
+    auto start = &v[0];
+    auto size = v.size();
+    auto end = start + size;
+
+    auto sumVector1 = std::async(std::launch::async, sumVectorA, start, (start + size / 4), 0);
+    auto sumVector2 = std::async(std::launch::async, sumVectorA, (start + size / 4), (start + size / 2), 0);
+    auto sumVector3 = std::async(std::launch::async, sumVectorA, (start + size * 3 / 4), (start + size), 0);
+    auto sumVector4 = std::async(std::launch::async, sumVectorA, (start + size * 3 / 4), (start + size), 0);
+
+    double total = sumVector1.get() + sumVector2.get() + sumVector3.get() + sumVector4.get();
+    return total;
+}
+
 int main() {
     IRandom rand(1, 100);
     std::vector<int> data;
-    for (auto i = 0; i < 10; i++) {
+    for (auto i = 0; i < 1000000; i++) {
         int n = rand();
         data.push_back(n);
     }
@@ -40,4 +71,19 @@ int main() {
     auto eTimer1 = std::chrono::high_resolution_clock::now();
     auto duration1 = std::chrono::duration_cast<std::chrono::microseconds>(eTimer1 - sTimer1);
     std::cout << "Execution Time (No Partitions): " << duration1.count() << " ms. " << std::endl;
+
+    auto sTimer2 = std::chrono::high_resolution_clock::now();
+    double r2 = sumVectorAsyncH(data);
+    std::cout << "Sum of Vector: " << r2 << std::endl;
+    auto eTimer2 = std::chrono::high_resolution_clock::now();
+    auto duration2 = std::chrono::duration_cast<std::chrono::microseconds>(eTimer2 - sTimer2);
+    std::cout << "Execution Timer (2 Partitions): " << duration2.count() << " ms. " << std::endl;
+
+    auto sTimer3 = std::chrono::high_resolution_clock::now();
+    double r3 = sumVectorAsyncQ(data);
+    std::cout << "Sum of Vector: " << r3 << std::endl;
+    auto eTimer3 = std::chrono::high_resolution_clock::now();
+    auto duration3 = std::chrono::duration_cast<std::chrono::microseconds>(eTimer3 - sTimer3);
+    std::cout << "Execution Timer (4 Partitions): " << duration3.count() << " ms. " << std::endl;
+
 }
